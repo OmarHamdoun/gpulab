ba85fd0cd2b5e763dd5af21378c975a7a5b08470
Stefan <sbtmp@online.de>
2013-03-27 17:35:51 +0100

    - implemented atomic forward warping kernel - cleaned up linear operations

--- superresolution_task/src/superresolution/superresolution.cu
+++ superresolution_task/src/superresolution/superresolution.cu
@@ -47,33 +47,34 @@ extern __shared__ float smem[];
 
 void computeSuperresolutionUngerGPU
 (
-		float *xi1_g, 							//! Dual Variable for TV regularization in X direction
-		float *xi2_g,							//! Dual Variable for TV regularization in X direction
-		float *temp1_g,							//! Helper array
+		float *xi1_g, 							// Dual Variable for TV regularization in X direction
+		float *xi2_g,							// Dual Variable for TV regularization in X direction
+		float *temp1_g,							// Helper array
 		float *temp2_g,
 		float *temp3_g,
 		float *temp4_g,
-		float *uor_g,							//! Field of overrelaxed primal variables
-		float *u_g,								//! GPU memory for the result image
-		std::vector<float*> &q_g,				//! Dual variables for L1 difference penalization
-		std::vector<float*> &images_g,			//! Dual variables for L1 difference penalization
-		std::list<FlowGPU> &flowsGPU,			//! GPU memory for the displacement fields
-		int   &nx,								//! New High-Resolution Width
-		int   &ny,								//! New High-Resolution Height
-		int   &pitchf1,							//! GPU pitch (padded width) of the superresolution high-res fields
-		int   &nx_orig,							//! Original Low-Resolution Width
-		int   &ny_orig,							//! Original Low-Resolution Height
-		int   &pitchf1_orig,					//! GPU pitch (padded width) of the original low-res images
-		int   &oi,								//! Number of Iterations
-		float &tau_p,							//! Primal Update Step Size
-		float &tau_d,							//! Dual Update Step Size
-		float &factor_tv,						//! The weight of Total Variation Penalization
-		float &huber_epsilon,					//! Parameter for Huber norm regularization
-		float &factor_rescale_x,				//! High-Resolution Width divided by Low-Resolution Width
-		float &factor_rescale_y,				//! High-Resolution Height divided by Low-Resolution Height
-		float &blur,							//! The amount of Gaussian Blur present in the degrading process
-		float &overrelaxation,					//! Overrelaxation parameter in the range of [1,2]
-		int   debug								//! Debug Flag, if activated the class produces Debug output.
+		float *uor_g,							// Field of overrelaxed primal variables
+		float *u_g,								// GPU memory for the result image
+		std::vector<float*> &q_g,				// Dual variables for L1 difference penalization
+		std::vector<float*> &images_g,			// Dual variables for L1 difference penalization
+		std::list<FlowGPU> &flowsGPU,			// GPU memory for the displacement fields
+												//   class FlowGPU { void clear(); float *u_g; float *v_g; int nx; int ny; }
+		int   &nx,								// New High-Resolution Width
+		int   &ny,								// New High-Resolution Height
+		int   &pitchf1,							// GPU pitch (padded width) of the superresolution high-res fields
+		int   &nx_orig,							// Original Low-Resolution Width
+		int   &ny_orig,							// Original Low-Resolution Height
+		int   &pitchf1_orig,					// GPU pitch (padded width) of the original low-res images
+		int   &oi,								// Number of Iterations
+		float &tau_p,							// Primal Update Step Size
+		float &tau_d,							// Dual Update Step Size
+		float &factor_tv,						// The weight of Total Variation Penalization
+		float &huber_epsilon,					// Parameter for Huber norm regularization
+		float &factor_rescale_x,				// High-Resolution Width divided by Low-Resolution Width
+		float &factor_rescale_y,				// High-Resolution Height divided by Low-Resolution Height
+		float &blur,							// The amount of Gaussian Blur present in the degrading process
+		float &overrelaxation,					// Overrelaxation parameter in the range of [1,2]
+		int   debug								// Debug Flag, if activated the class produces Debug output.
 )
 {
 	// replacing u by u_g ( pointer to resultant data)
@@ -112,20 +113,15 @@ void computeSuperresolutionUngerGPU
 	{
 		fprintf(stderr," %i",i);
 
-		//TODO: KERNEL FOR DUAL TV
-		//dualTVHuber(_u_overrelaxed,_xi1,_xi2,_nx,_ny,factor_tv_update,factor_tv_clipping,huber_denom_tv,_tau_d);
+		// TODO: KERNEL FOR DUAL TV
+		// dualTVHuber(_u_overrelaxed,_xi1,_xi2,_nx,_ny,factor_tv_update,factor_tv_clipping,huber_denom_tv,_tau_d);
 
-		//DUAL DATA		
+		// DUAL DATA		
 		
-		// NEED TO INITIALISE A ITERATOR FOR ORIGINAL IMAGES
-		//std::vector<cv::Mat*>::iterator image = _images_original.begin();
-		
-		// NEED TO SET A ITERATOR FOR FLOWS
-		//std::list<Flow>::iterator flow = _flows.begin();
-				
+		// iterating over all images
 		std::vector<float*>::iterator image = images_g.begin();
 		std::list<FlowGPU>::iterator flow   = flowsGPU.begin();
-		for( int k = 0; image != images_g.end() && flow != flowsGPU.end() && k < q_g.size(); ++k, ++flow, ++image )		
+		for( unsigned int k = 0; image != images_g.end() && flow != flowsGPU.end() && k < q_g.size(); ++k, ++flow, ++image )		
 		{
 				// TODO: KERNEL BACKWARDREGISTRATIONBILINEARVALUE
 				
@@ -160,11 +156,13 @@ void computeSuperresolutionUngerGPU
 				
 		}
 		
-		// TODO: KERNEL TO SET 3RD HELPER ARRAY TO 0.00f
+		// set 3rd helper array to zero
+		setKernel <<<dimGrid, dimBlock>>>( temp3_g, nx, ny, pitchf1, 0.0f );
 
+		// iterating over all images
 		image = images_g.begin();
 		flow   = flowsGPU.begin();
-		for( int k = 0; image != images_g.end() && flow != flowsGPU.end() && k < q_g.size(); ++k, ++flow, ++image )
+		for( unsigned int k = 0; image != images_g.end() && flow != flowsGPU.end() && k < q_g.size(); ++k, ++flow, ++image )
 		{
 			if( factor_rescale_x > 1.0f || factor_rescale_y > 1.0f )
 			{
@@ -178,8 +176,7 @@ void computeSuperresolutionUngerGPU
 			
 			if( blur > 0.0f )
 			{
-				// TODO: KERNEL FOR GAUSSBLURSEPARATEMIRROR
-				// lookout for change in parameters, if any
+				// TODO: KERNEL FOR GAUSSBLURSEPARATEMIRROR (lookout for change in parameters, if any)
 			}
 			else
 			{
@@ -189,7 +186,13 @@ void computeSuperresolutionUngerGPU
 				temp2_g = temp;
 			}
 			
-			// TODO: IMPLEMENT forewardRegistrationBilinear
+			// foreward warping
+			forewardRegistrationBilinearAtomic (
+					flow->u_g, flow->v_g,
+					temp2_g, temp1_g,
+					nx, ny,
+					pitchf1
+				);
 			
 			// add 1st to 3rd helper array
 			addKernel <<<dimGrid, dimBlock>>>( temp1_g, temp3_g, nx, ny, pitchf1 );
